08c68b55f2744e2111cb30b2a8e0ade38073a5cd
Eleazar Garcia <elgarcia@hacienda.gov.do>
2023-01-27 11:01:23 -0400

    validaciones y optimizaciones

---
+++ src/todos/use-cases/render-todos.js
@@ -0,0 +1,23 @@
+import { Todo } from '../models/todo.models';
+import { createTodoHTML } from './'
+
+let element;
+/**
+ * 
+ * @param {*} elementId 
+ * @param {Todo} todos 
+ */
+
+export const renderTodos = ( elementId, todos = []) => {
+
+    if (!element)
+        element = document.querySelector(elementId);
+
+    if ( !element ) throw new Error(`Element ${ elementId } not found`);
+    
+    element.innerHTML = '';
+
+    todos.forEach(todo => {
+        element.append( createTodoHTML(todo) )
+    });
+}
